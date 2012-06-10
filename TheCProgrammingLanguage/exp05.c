bfb57f375777b16eb797a364c50d111e1eabe5bb
Geng Shuang <qh997@hotmail.com>
2012-06-10 22:39:44 +0800

    Study TCPL Exp5.6

---
+++ TheCProgrammingLanguage/exp05.c
@@ -0,0 +1,34 @@
+#include "expxx.h"
+
+int main(void)
+{
+    print_title("exp05");
+
+    return 0;
+}
+
+#define ALLOCSIZE 10000
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp= allocbuf;
+
+char *alloc(int n)
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n)
+    {
+        allocp += n;
+        return allocp - n;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+void afree(char *p)
+{
+    if (p >= allocbuf && a < allocbuf + ALLOCSIZE)
+    {
+        allocp = p;
+    }
+}
