5b7264ac1b419085f3688ef57ac54ff202e3eddf
Geng Shuang <gengs@neusoft.com>
2012-06-11 16:25:47 +0800

    第五章 指针与数组

--- TheCProgrammingLanguage/exp05.c
+++ TheCProgrammingLanguage/exp05.c
@@ -1,9 +1,17 @@
 #include "expxx.h"
 #include <float.h>
 
+int day_of_year(int year, int month, int day);
+
 int main(void)
 {
     print_title("exp05");
+
+    print_title("Multidimensional array");
+    {
+        show_var("%d", day_of_year(2012, 3, 31));
+        show_var("%d", day_of_year(2012, 6, 11));
+    }
 
     return 0;
 }
@@ -32,4 +40,20 @@ void afree(char *p)
     {
         allocp = p;
     }
+}
+
+static char daytab[2][13] = 
+{
+    {0,31,28,31,30,31,30,31,31,30,31,30,31},
+    {0,31,29,31,30,31,30,31,31,30,31,30,31}
+};
+
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    for (i = 1; i < month; i++)
+        day += *(*(daytab + leap) + i);
+    return day;
 }
